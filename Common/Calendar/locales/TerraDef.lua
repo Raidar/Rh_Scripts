79d097d95ab8f8a46b5691236358cf566005fa91
Aidar Rakhmatullin <raidar@yandex.ru>
2013-03-02 12:07:32 +0400

    Add some fete days

--- Common/Calendar/locales/TerraDef.lua
+++ Common/Calendar/locales/TerraDef.lua
@@ -3,14 +3,30 @@
 --------------------------------------------------------------------------------
 local Data = {
   ----------------------------------------
-  VictoryDay                = "Victory Day",
-  GreatPatrioticWarStart    = "Start of the Great Patriotic War",
+  -- Common fests:
+  NewYear                   = "New Year",                   -- N.Y.   -- 01-01
+  SaintValentineDay         = "Saint Valentine's Day",      -- SVD    -- 02-14
+  FatherlandDayDefender     = "Defender of the Fatherland Day",       -- 02-23
+  InternationalWomensDay    = "International Women's Day",  -- IWD    -- 03-08
+  AllFoolsDay               = "All Fools' Day",             -- AFD    -- 04-01
+  InterAnimationDay         = "International Animation Day",-- IAD    -- 10-28
+  InternationalMensDay      = "International Men's Day",    -- IMD    -- 11-19
+  NewYearsEve               = "New Year's Eve",             -- NYE    -- 12-31
 
-  SaintValentineDay         = "Saint Valentine's Day",
-  FatherlandDayDefender     = "Defender of the Fatherland Day",
-  InternationalMensDay      = "International Men's Day",
-  InternationalWomensDay    = "International Women's Day",
+  -- Special days:
+    -- End of the Great Patriotic War
+  GPW_VictoryDay            = "Victory Day",                -- V-Day  -- 05-09
+    -- Start of the Great Patriotic War
+  GPW_MemorialDay           = "Memorial Day",               -- M-day  -- 06-22
 
+  -- Professional days:
+    -- International Day of Human Space Flight
+  WorldCosmonauticsDay      = "World Cosmonautics Day",               -- 04-12
+  SystemAdminsDay           = "System Administrator Appreciation Day",-- 07
+  ProgrammersDay            = "Programmers' Day",                     -- 09-12/13
+  UpbringersDay             = "Upbringers' Day",            -- ???    -- 09-27
+  WorldTeachersDay          = "World Teachers' Day",                  -- 10-05
+  InterAccountantsDay       = "International Accountants' Day",       -- 11-10
   ----------------------------------------
 } --- Data
 
