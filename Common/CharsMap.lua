6e21578e45c17258d341a5a7b22ca418f8b2d049
Aidar Rakhmatullin <raidar@yandex.ru>
2013-04-22 21:14:30 +0400

    First worked version

---
+++ Common/CharsMap.lua
@@ -0,0 +1,748 @@
+--[[ CharsMap ]]--
+
+----------------------------------------
+--[[ description:
+  -- Menu based characters map.
+  -- Таблица символов на основе меню.
+--]]
+----------------------------------------
+--[[ uses:
+  LuaFAR,
+  LF context,
+  Rh Utils.
+  -- group: Common.
+  -- areas: any.
+--]]
+--------------------------------------------------------------------------------
+
+local type = type
+local pairs = pairs
+local setmetatable = setmetatable
+
+----------------------------------------
+local useprofiler = false
+--local useprofiler = true
+
+if useprofiler then
+  require "profiler" -- Lua Profiler
+  profiler.start("CharsMap.log")
+end
+
+----------------------------------------
+--local win = win
+--local win, far = win, far
+
+----------------------------------------
+--local context = context
+local logShow = context.ShowInfo
+
+--local utils = require 'context.utils.useUtils'
+local numbers = require 'context.utils.useNumbers'
+local strings = require 'context.utils.useStrings'
+local tables = require 'context.utils.useTables'
+local datas = require 'context.utils.useDatas'
+local locale = require 'context.utils.useLocale'
+
+local divf = numbers.divf
+
+local uCP = strings.ucp2s
+
+local Null = tables.Null
+local addNewData = tables.extend
+
+----------------------------------------
+local farUt = require "Rh_Scripts.Utils.Utils"
+
+----------------------------------------
+local keyUt = require "Rh_Scripts.Utils.Keys"
+
+local IsModCtrl, IsModAlt = keyUt.IsModCtrl, keyUt.IsModAlt
+local IsModCtrlAlt = keyUt.IsModCtrlAlt
+
+----------------------------------------
+local CharsList = require "Rh_Scripts.Utils.CharsList"
+local uCodeName = CharsList.uCodeName
+
+--------------------------------------------------------------------------------
+local unit = {}
+
+---------------------------------------- Main data
+unit.ScriptName = "CharsMap"
+unit.ScriptPath = "scripts\\Rh_Scripts\\Common\\"
+
+local usercall = farUt.usercall
+unit.RunMenu = usercall(nil, require, "Rh_Scripts.RectMenu.RectMenu")
+
+---------------------------------------- ---- Custom
+unit.DefCustom = {
+  name = unit.ScriptName,
+  path = unit.ScriptPath,
+
+  label = unit.ScriptName,
+
+  help   = { topic = unit.ScriptName, },
+  locale = {
+    kind = 'load',
+  }, --
+} -- DefCustom
+
+----------------------------------------
+unit.DefOptions = {
+  BaseDir  = "Rh_Scripts.Common."..unit.ScriptName..".",
+} -- DefOptions
+
+--[[
+local L, e1, e2 = locale.localize(unit.DefCustom)
+if L == nil then
+  return locale.showError(e1, e2)
+end
+
+logShow(L, "L", "wM")
+--]]
+---------------------------------------- ---- Config
+unit.DefCfgData = { -- Конфигурация по умолчанию:
+} -- DefCfgData
+
+---------------------------------------- ---- Types
+--unit.DlgTypes = { -- Типы элементов диалога:
+--} -- DlgTypes
+
+---------------------------------------- Configure
+
+---------------------------------------- Main class
+local TMain = {
+  --Guid       = win.Uuid(""),
+  --ConfigGuid = win.Uuid(""),
+}
+local MMain = { __index = TMain }
+
+-- Создание объекта основного класса.
+local function CreateMain (ArgData)
+
+  local self = {
+    ArgData   = addNewData(ArgData, unit.DefCfgData),
+
+    Custom    = false,
+    Options   = false,
+    History   = false,
+    CfgData   = false,
+    --DlgTypes  = unit.DlgTypes,
+    LocData   = false,
+    L         = false,
+
+    RowCount = false,     -- Количество строк меню
+    ColCount = false,     -- Количество стобцов меню
+
+    -- Текущее состояние:
+    Items     = false,    -- Список пунктов меню
+    Props     = false,    -- Свойства меню
+
+    CharRows  = 0x08,     -- Количество строк символов
+    --CharRows  = 0x10,     -- Количество строк символов
+    CharCols  = 0x10,     -- Количество столбцов символов
+    CharCount = 0x100,    -- Количество всех символов
+
+    CharMin   = 0x0000,   -- Минимальный символ
+    CharMax   = 0xFFFD,   -- Максимальный символ
+    --CharBase  = 0x2000,   -- Базовый символ
+    --CharBase  = 0x0000,   -- Базовый символ
+    DigitNum  = 4,        -- Количество цифр в CodePoint символа
+
+    ActItem   = false,    -- Выбранный пункт меню
+    ItemPos   = false,    -- Позиция выбранного пункта меню
+    --Action    = false,    -- Выбранное действие
+    --Effect    = false,    -- Выбранный эффект
+  } --- self
+
+  self.ArgData.Custom = self.ArgData.Custom or {} -- MAYBE: addNewData with deep?!
+  --logShow(self.ArgData, "ArgData", "wA d2")
+  self.Custom = datas.customize(self.ArgData.Custom, unit.DefCustom)
+  self.Options = addNewData(self.ArgData.Options, unit.DefOptions)
+
+  self.History = datas.newHistory(self.Custom.history.full)
+  self.CfgData = self.History:field(self.Custom.history.field)
+
+  --local CfgData = self.CfgData
+  --self.Menu = CfgData.Menu or {}
+  --self.Menu.CompleteKeys = self.Menu.CompleteKeys or unit.CompleteKeys
+  --self.Menu.LocalUseKeys = self.Menu.LocalUseKeys or unit.LocalUseKeys
+
+  setmetatable(self.CfgData, { __index = self.ArgData })
+  --logShow(self.CfgData, "CfgData", "w")
+
+  locale.customize(self.Custom)
+  --logShow(self.Custom, "Custom")
+
+  return setmetatable(self, MMain)
+end -- CreateMain
+
+---------------------------------------- Dialog
+
+---------------------------------------- Main making
+
+---------------------------------------- ---- Init
+do
+  local max, min = math.max, math.min
+
+function TMain:InitData ()
+  local self = self
+  --local CfgData = self.CfgData
+
+  self.CharCount = self.CharRows * self.CharCols
+  self.CharBase = min(max(self.CharBase or 0x0000, self.CharMin), self.CharMax)
+
+  return true
+end ---- InitData
+
+  local divf = numbers.divf
+
+-- Инициализация символа.
+function TMain:InitChar ()
+  local self = self
+
+  local CfgData = self.CfgData
+  --logShow(CfgData, "CfgData")
+
+  local Char = CfgData.Char or self.CharBase
+  self.Char, self.DefChar = Char, Char
+  self.CharBase = divf(Char, self.CharCount) * self.CharCount --> TODO: FixCharBase!
+  --logShow(self, "TMain", "w d1")
+
+  return true
+end ---- InitChar
+
+end -- do
+---------------------------------------- ---- Prepare
+do
+-- Localize data.
+-- Локализация данных.
+function TMain:Localize ()
+  local self = self
+
+  self.LocData = locale.getData(self.Custom)
+  -- TODO: Нужно выдавать ошибку об отсутствии файла сообщений!!!
+  if not self.LocData then return end
+
+  self.L = locale.make(self.Custom, self.LocData)
+
+  return self.L
+end ---- Localize
+
+function TMain:MakeColors ()
+
+  local colors = require 'context.utils.useColors'
+  local basics = colors.BaseColors
+  local Basis = {
+    StandardFG  = basics.navy,
+    --MarkedFG    = basics.red,
+    MarkedFG    = basics.black,
+    BorderFG    = basics.black,
+    TitleFG     = basics.black,
+    --StatusBarFG = basics.black,
+    ScrollBarFG = basics.black,
+  } --- Basis
+
+  local menUt = require "Rh_Scripts.Utils.Menu"
+  self.Colors = menUt.FormColors(Basis)
+
+  return true
+end ---- MakeColors
+
+function TMain:MakeProps ()
+  local self = self
+
+  self.RowCount = 1 + self.CharRows + 1
+  self.ColCount = 1 + self.CharCols + 1
+
+  -- Свойства меню:
+  local Props = self.CfgData.Props or {}
+  self.Props = Props
+
+  Props.Id = Props.Id or self.Guid
+  Props.HelpTopic = self.Custom.help.tlink
+
+  local L = self.LocData
+  Props.Title  = L.CharsMap
+  --Props.Bottom = L.CharsMapKeys
+
+  -- Свойства RectMenu:
+  local RM_Props = {
+    Order = "H",
+    --Rows = self.RowCount,
+    Cols = self.ColCount,
+
+    Fixed = {
+      HeadRows = 1,
+      FootRows = 1,
+      HeadCols = 1,
+      FootCols = 1,
+    },
+
+    --MenuEdge = 2,
+    MenuAlign = "CM",
+
+    LineMax = 1,
+    TextMax = {
+      [0] = 1,
+      [1] = self.DigitNum,
+      [self.ColCount] = self.DigitNum,
+    }, --
+
+    MaxHeight = 2 + self.ColCount + 2 + 0,
+
+    Colors = self.Colors,
+
+    --RectItem = {
+    --  TextMark = true,
+    --},
+  } --- RM_Props
+  Props.RectMenu = RM_Props
+
+  self.RectItem = {
+    TextMark = true,
+  } --
+
+  return true
+end ---- MakeProps
+
+local Msgs = {
+  NoLocale  = "No localization",
+} ---
+
+-- Подготовка.
+-- Preparing.
+function TMain:Prepare ()
+
+  self:InitData()
+
+  self:InitChar()
+
+  if not self:Localize() then
+    --self.Error = Msgs.NoLocale
+    --return
+    self.LocData = {} -- DEBUG only
+  end
+  
+  self:MakeColors()
+
+  return self:MakeProps()
+end ---- Prepare
+
+end -- do
+---------------------------------------- ---- Menu
+do
+-- Заполнение меню.
+function TMain:FillMenu () --> (table)
+  local self = self
+
+  local CharRows, CharCols = self.CharRows, self.CharCols
+  local RowCount, ColCount = self.RowCount, self.ColCount
+
+  local t = self.Items
+
+  local Filler = "────"
+  local l = (RowCount - 1) * ColCount + 1
+  t[1].text                 = Filler
+  t[1 + ColCount - 1].text  = Filler
+  t[l].text                 = Filler
+  t[l + ColCount - 1].text  = Filler
+
+  local Order = "0123456789ABCDEF"
+  for k = 1, CharCols do
+    local s = Order:sub(k, k)
+    t[1 + k].text = s
+    t[l + k].text = s
+  end
+
+  local p = 0
+  local b = self.CharBase
+  for k = 1, CharRows do
+    p = p + ColCount
+    local s = uCP(b, true)
+    t[p + 1].text        = s
+    t[p + ColCount].text = s
+    b = b + CharCols
+  end
+
+  p = 1                 -- Индекс начального пункта строки
+  b = self.CharBase     -- Код символа для текущего пункта
+  local U = unicode.utf8.char
+
+  local SelChar = self.Char -- Код текущего символа
+  local SelIndex            -- Индекс пункта с текущим символом
+
+  for i = 1, CharRows do
+    p = p + ColCount
+
+    for j = 1, CharCols do
+      local c = U(b)
+      local f = t[p + j]
+
+      f.text = c
+      f.Hint = uCodeName(b)
+      f.RectMenu = self.RectItem
+
+      f.Data = {
+        Char = b,
+        char = c,
+        r = i,
+        c = j,
+        --p = p + j,
+      } --
+      
+      if not SelIndex and b == SelChar then
+        SelIndex = p + j
+      end
+
+      b = b + 1
+    end
+  end --
+
+  self.Props.SelectIndex = SelIndex
+
+  return true
+end ---- FillMenu
+
+-- Формирование меню.
+function TMain:MakeMenu () --> (table)
+  local self = self
+
+  local RowCount, ColCount = self.RowCount, self.ColCount
+
+  local t = {}
+  self.Items = t
+
+  -- Формирование пунктов:
+  for _ = 1, ColCount do
+    for _ = 1, RowCount do
+      t[#t+1] = {
+        text = "",
+        Label = true,
+      } --
+    end
+  end --
+
+  self:FillMenu() -- Таблица символов
+
+  return true
+end -- MakeMenu
+
+-- Формирование календаря.
+function TMain:Make () --> (table)
+
+  self.Items = false -- Сброс меню (!)
+
+  return self:MakeMenu()
+end -- Make
+
+end -- do
+---------------------------------------- ---- Utils
+
+---------------------------------------- ---- Input
+do
+--[[
+  local tonumber = tonumber
+  local InputFmtY   = "^(%d+)"
+  local InputFmtYM  = "^(%d+)%-(%d+)"
+  local InputFmtYMD = "^(%d+)%-(%d+)%-(%d+)"
+
+function TMain:ParseInput ()
+  local self = self
+  local Date = self.Date:copy()
+
+  local Input = self.Input or ""
+  local _, Count = Input:gsub("-", "")
+
+  if Count == 0 then
+    local y = Input:match(InputFmtY)
+    y = tonumber(y) or 1
+    Date.y, Date.m, Date.d = y, 1, 1
+  elseif Count == 1 then
+    local y, m = Input:match(InputFmtYM)
+    y, m = tonumber(y) or 1, tonumber(m) or 1
+    Date.y, Date.m, Date.d = y, m, 1
+  else
+    local y, m, d = Input:match(InputFmtYMD)
+    y, m, d = tonumber(y) or 1, tonumber(m) or 1, tonumber(d) or 1
+    --logShow({ Count, y, m, d }, Input)
+    Date.y, Date.m, Date.d = y, m, d
+  end
+
+  return Date:fixMonth():fixDay()
+end ---- ParseInput
+
+function TMain:StartInput (Date)
+  local self = self
+  local L = self.LocData
+
+  self.Input = ""
+  self.Props.Bottom = L.InputDate
+  self.IsInput = true
+end ---- StartInput
+
+function TMain:StopInput (Date)
+  local self = self
+
+  self.IsInput = false
+  self.Props.Bottom = ""
+  self.Date = self:ParseInput() or Date
+end ---- StopInput
+
+function TMain:EditInput (SKey)
+  local self = self
+
+  local Input = self.Input
+  if SKey == "BS" then
+    if Input ~= "" then Input = Input:sub(1, -2) end
+  else
+    if SKey == "Subtract" then SKey = "-" end
+
+    Input = Input..SKey
+  end
+
+  self.Input = Input
+  self.Props.Bottom = Input
+end ---- EditInput
+--]]
+
+-- Вывод символа.
+function TMain:PrintChar (Data)
+  --logShow(Data, "PrintChar")
+  if type(Data.char) == 'string' then
+    return farUt.InsertText(nil, Data.char)
+  end
+end ---- PrintChar
+
+-- Сохранение данных.
+function TMain:SaveData (Data)
+  local self = self
+
+  self.CfgData.Char = Data.Char
+
+  return self.History:save()
+end ---- SaveData
+
+end -- do
+---------------------------------------- ---- Events
+do
+  local CloseFlag  = { isClose = true }
+  local CancelFlag = { isCancel = true }
+  local CompleteFlags = { isRedraw = false, isRedrawAll = true }
+
+  local max, min = math.max, math.min
+
+function TMain:AssignEvents () --> (bool | nil)
+  local self = self
+  
+  local function MakeUpdate () -- Обновление!
+    farUt.RedrawAll()
+    self:Make()
+    --logShow(self.Items, "MakeUpdate")
+    if not self.Items then return nil, CloseFlag end
+    --logShow(ItemPos, hex(FKey))
+    return { self.Props, self.Items, self.Keys }, CompleteFlags
+  end -- MakeUpdate
+
+--[[
+  -- Обработчик нажатия клавиш.
+  local function KeyPress (VirKey, ItemPos)
+    local SKey = VirKey.Name --or InputRecordToName(VirKey)
+    if SKey == "Esc" then return nil, CancelFlag end
+    --logShow(SKey, "SKey")
+
+    --local DT_cfg = self.DT_cfg
+    local Data = self.Items[ItemPos].Data
+    if not Data then return end
+
+    local Date = Data[Data.State]
+    if Data.d <= 0 then return end
+    Date.d = Data.d
+
+    local isUpdate = true
+    local Action = DateActions[SKey]
+    if Action then
+      Date[Action](Date)
+
+    elseif SKey == "Divide" then
+      if self.IsInput then
+        self:StopInput(Date)
+        return MakeUpdate()
+      else
+        self:StartInput(Date)
+      end
+
+    elseif self.IsInput and InputActions[SKey] then
+      self:EditInput(SKey)
+
+    else
+      isUpdate = false
+    end
+
+    self.Time = false
+
+    if isUpdate then
+      --self.Date = Date
+      self.Date = self:LimitDate(Date)
+      return MakeUpdate()
+    end
+
+    return
+  end -- KeyPress
+--]]
+
+  -- Обработчик нажатия клавиш навигации.
+  local function NavKeyPress (AKey, VMod, ItemPos)
+    local AKey, VMod = AKey, VMod
+
+    local Data = self.Items[ItemPos].Data
+    if not Data then return end
+
+    local isUpdate = true
+    if VMod == 0 then
+      --logShow({ AKey, VMod, Date }, ItemPos, "w d2")
+      if AKey == "Clear" or AKey == "Multiply" then
+        self.Char = self.DefChar
+      elseif AKey == "Left"  and Data.c == 1 then
+        self.Char = Data.Char - 1
+      elseif AKey == "Right" and Data.c == self.CharCols then
+        self.Char = Data.Char + 1
+      elseif AKey == "Up"    and Data.r == 1 then
+        self.Char = Data.Char - self.CharCols
+      elseif AKey == "Down"  and Data.r == self.CharRows then
+        self.Char = Data.Char + self.CharCols
+      elseif AKey == "PgUp" or
+             (AKey == "Home" and
+              Data.r == 1 and Data.c == 1) then
+        self.Char = Data.Char - self.CharCount
+      elseif AKey == "PgDn" or
+             (AKey == "End" and
+              Data.r == self.CharRows and Data.c == self.CharCols) then
+        self.Char = Data.Char + self.CharCount
+      else
+        isUpdate = false
+      end
+
+    elseif IsModCtrl(VMod) then -- CTRL
+      if AKey == "Clear" or AKey == "Multiply" then
+        self.Char = self.DefChar
+      elseif AKey == "Home" then
+        self.Char = self.CharMin
+      elseif AKey == "End" then
+        self.Char = self.CharMax
+      else
+        isUpdate = false
+      end
+
+    --elseif IsModAlt(VMod) then -- ALT
+
+    --elseif IsModCtrlAlt(VMod) then -- ALT+CTRL
+
+    else
+      isUpdate = false
+    end
+
+    if isUpdate then
+      -- TODO: Выделить в отдельную функцию !!! для кода ниже и в InitData
+      self.Char = min(max(self.Char, self.CharMin), self.CharMax)
+      self.CharBase = divf(self.Char, self.CharCount) * self.CharCount
+
+      return MakeUpdate()
+    end
+
+    return
+  end -- NavKeyPress
+
+--[[
+  -- Обработчик выделения пункта.
+  local function SelectItem (Kind, ItemPos)
+    local Data = self.Items[ItemPos].Data
+    --logShow(Data, ItemPos, "w d2")
+    if not Data then return end
+
+    self.Date = Data[Data.State]
+    if Data.d <= 0 then return end
+    self.Date.d = Data.d
+
+    return self:FillInfoPart()
+  end -- SelectItem
+--]]
+
+  -- Обработчик выбора пункта.
+  local function ChooseItem (Kind, ItemPos)
+    local Data = self.Items[ItemPos].Data
+    if not Data then return end
+
+    self:SaveData(Data)
+
+    return nil, CloseFlag
+  end -- ChooseItem
+
+  -- Назначение обработчиков:
+  local RM_Props = self.Props.RectMenu
+  --RM_Props.OnKeyPress = KeyPress
+  RM_Props.OnNavKeyPress = NavKeyPress
+  --RM_Props.OnSelectItem = SelectItem
+  RM_Props.OnChooseItem = ChooseItem
+end -- AssignEvents
+
+end --
+---------------------------------------- ---- Action
+
+function TMain:Apply () --> (item, pos)
+  return self:PrintChar(self.ActItem.Data)
+end ---- Apply
+
+---------------------------------------- ---- Show
+-- Показ меню заданного вида.
+function TMain:ShowMenu () --> (item, pos)
+  return usercall(nil, unit.RunMenu, self.Props, self.Items, self.Keys)
+end ---- ShowMenu
+
+-- Вывод календаря.
+function TMain:Show () --> (bool | nil)
+
+  --local Cfg = self.CfgData
+
+  self:Make()
+  if self.Error then return nil, self.Error end
+
+  if useprofiler then profiler.stop() end
+
+  self.ActItem, self.ItemPos = self:ShowMenu()
+  if self.ActItem then return self:Apply() end
+
+  return true
+end -- Show
+
+---------------------------------------- ---- Run
+function TMain:Run () --> (bool | nil)
+
+  self:AssignEvents()
+
+  return self:Show()
+end -- Run
+
+---------------------------------------- main
+
+function unit.Execute (Data) --> (bool | nil)
+
+  --logShow(Data, "Data", "w d2")
+
+  local _Main = CreateMain(Data)
+  if not _Main then return end
+
+  --logShow(Data, "Data", "w d2")
+  --logShow(_Main, "Config", "w _d2")
+  --logShow(_Main.CfgData, "CfgData", "w d2")
+  --if not _Main.CfgData.Enabled then return end
+
+  _Main:Prepare()
+  if _Main.Error then return nil, _Main.Error end
+
+  return _Main:Run()
+end ---- Execute
+
+--------------------------------------------------------------------------------
+--return unit
+return unit.Execute()
+--------------------------------------------------------------------------------
