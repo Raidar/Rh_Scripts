1a4023b534d1e6411c5b6503f8358cda99e5f505
Aidar Rakhmatullin <raidar@yandex.ru>
2013-11-26 01:41:24 +0400

    Move chars processing to CharsData.lua

---
+++ Utils/CharsData.lua
@@ -0,0 +1,313 @@
+--[[ Characters' data ]]--
+
+----------------------------------------
+--[[ description:
+  -- Unicode characters' data.
+  -- Данные по Unicode‑символам.
+--]]
+----------------------------------------
+--[[ uses:
+  LuaFAR.
+  -- group: Utils.
+--]]
+--------------------------------------------------------------------------------
+
+local tonumber = tonumber
+local setmetatable = setmetatable
+
+local io_open = io.open
+
+----------------------------------------
+--local context = context
+local logShow = context.ShowInfo
+
+local utils = require 'context.utils.useUtils'
+local strings = require 'context.utils.useStrings'
+
+--------------------------------------------------------------------------------
+local unit = {}
+
+---------------------------------------- Main data
+unit.ScriptName = "CharsList"
+unit.ScriptPath = "scripts\\Rh_Scripts\\Utils\\"
+
+---------------------------------------- ---- Config
+
+unit.DefCfgData = { -- Конфигурация по умолчанию:
+  PluginPath = utils.PluginPath,
+  FilePath  = "scripts\\Rh_Scripts\\data\\",
+  NamesFile     = "NamesList.txt",
+  BlocksFile    = "Blocks.txt",
+
+  DataFile      = "CharsData.lua",
+  DataPath      = "Rh_Scripts.data.CharsData",
+} -- DefCfgData
+
+---------------------------------------- Main class
+
+---------------------------------------- Naming
+do --- Преобразование в читаемый формат
+
+  local lower = unicode.utf8.lower
+  local function _EasyName (head, tail) --> (string)
+    return head..lower(tail)
+  end --
+
+  local supper = string.upper
+
+function unit.EasyName (s, cp) --> (string)
+  return s:gsub("(%w)(%w+)", _EasyName):
+           gsub("([%dA-F][%da-f][%da-f][%da-f])",
+                function (s)
+                  if supper(s) == cp then return cp end
+                end)
+end -- EasyName
+
+end --
+---------------------------------------- Parse
+-- Load and parse NamesList.txt.
+-- Загрузка и разбор NamesList.txt.
+--[[
+  -- @params:
+  FileName (string) - name of loaded text file.
+  -- @return:
+  (bool|nil) - result of data parsing.
+--]]
+function unit:ParseNames (FileName) --> (bool | nil)
+
+  local FileName = FileName
+
+  local f = io_open(FileName, 'r')
+  if f == nil then return end
+  --[[
+  local f, SError = io_open(FileName, 'r')
+  if SError then logShow(SError, "NamesList.txt") end
+  --]]
+
+  -- Цикл по строкам файла:
+
+  local last = 0
+
+  local t = self.Data.Names
+  local easy = unit.EasyName
+
+  local s = "@"
+  --local sn = 0
+  repeat
+    --logShow(s, "File line")
+    -- Пропуск комментариев, доп. информации и старых названий:
+    while s and (s:find("^%@") or
+          s:find("^\t[^%=]") or
+          s:find("^\t%= .+%(1%.0%)$")) do
+      --logShow(s, "File unused line")
+      s = f:read('*l')
+      --sn = sn + 1
+    end
+    if s == nil then break end -- Конец файла
+    --if sn > 40 then break end -- DEBUG only
+    --logShow(s, "File used line")
+
+    -- TODO: Check codepoint ranges!!
+    -- Основное название:
+    local code, name = s:match("^(%x%x%x%x)\t(.+)$")
+    if code then
+      --logShow(code, name)
+      local cp = tonumber(code, 16)
+      --if cp > 0x300 then break end -- DEBUG only
+      --if cp > 0xFFFF then break end -- DEBUG only
+
+      t[cp] = {
+        code = code,
+        name = name:sub(1, 1) == '<' and name or easy(name),
+      } ---
+
+      last = cp
+    end
+
+    --[[
+    name = s:match("^\t%= (.+)")
+    -- Альтернативное название:
+    if name then
+      data = t[last]
+      if not data.alias then data.alias = {} end
+      data.alias[#data.alias+1] = name
+    end --
+    --]]
+
+    s = f:read('*l')
+    --sn = sn + 1
+  until s == nil
+
+  f:close()
+
+  return true
+end ---- ParseNames
+
+-- Load and parse Blocks.txt.
+-- Загрузка и разбор Blocks.txt.
+--[[
+  -- @params:
+  FileName (string) - name of loaded text file.
+  -- @return:
+  (bool|nil) - result of data parsing.
+--]]
+function unit:ParseBlocks (FileName) --> (bool | nil)
+
+  local FileName = FileName
+
+  local f = io_open(FileName, 'r')
+  if f == nil then return end
+  --[[
+  local f, SError = io_open(FileName, 'r')
+  if SError then logShow(SError, "NamesList.txt") end
+  --]]
+
+  -- Цикл по строкам файла:
+
+  local t = self.Data.Blocks
+
+  local s = "#"
+  local k = 0
+  --local sn = 0
+  repeat
+    --logShow(s, "File line")
+    -- Пропуск комментариев, доп. информации и старых названий:
+    while s and s:find("^%#") do
+      --logShow(s, "File unused line")
+      s = f:read('*l')
+      --sn = sn + 1
+    end
+    if s == nil then break end -- Конец файла
+    --if sn > 40 then break end -- DEBUG only
+    --logShow(s, "File used line")
+
+    -- TODO: Check codepoint ranges!!
+    -- Основное название:
+    local code1, code2, name = s:match("^(%x%x%x%x)%.%.(%x%x%x%x)%; (.+)$")
+    if code1 and code2 then
+      k = k + 1
+      --logShow(code, name)
+      local cp1 = tonumber(code1, 16)
+      local cp2 = tonumber(code2, 16)
+      --if cp > 0x300 then break end -- DEBUG only
+      --if cp > 0xFFFF then break end -- DEBUG only
+
+      t[k] = {
+        first = cp1,
+        last  = cp2,
+        --code1 = code1,
+        --code2 = code2,
+        name = name,
+      } ---
+    end
+
+    s = f:read('*l')
+    --sn = sn + 1
+  until s == nil
+
+  f:close()
+
+  return true
+end ---- ParseBlocks
+---------------------------------------- Load / Save
+do  
+  local farUt = require "Rh_Scripts.Utils.Utils"
+
+-- Load Data.
+-- Загрузка данных.
+--[[
+  -- @params:
+  FilePath (string) - path to loaded lua file.
+  -- @return:
+  (bool|nil) - result of data loading.
+--]]
+function unit:LoadData (FilePath) --> (bool | nil)
+  local Data = farUt.prequire(FilePath)
+  if Data then
+    self.Data = Data
+    return true
+  end
+end ---- LoadData
+
+-- Save Data.
+-- Сохранение данных.
+--[[
+  -- @params:
+  FileName (string) - name of loaded lua file.
+  -- @return:
+  (bool|nil) - result of data saving.
+--]]
+function unit:SaveData (FileName) --> (bool | nil)
+
+  --local tables = require 'context.utils.useTables'
+  local datas = require 'context.utils.useDatas'
+  local serial = require 'context.utils.useSerial'
+
+  local kind = {
+    --localret = true,
+    --tnaming = true,
+    astable = true,
+    --nesting = 0,
+
+    lining = "all",
+
+    serialize = serial.prettyize,
+  } ---
+
+  return datas.save(FileName, "Data", self.Data, kind)
+end ---- SaveData
+
+end --
+---------------------------------------- main
+do
+  local sformat = string.format
+
+function unit:Execute (Data) --> (bool | nil)
+
+  local CfgData = Data or {}
+  local CfgMeta = { __index = self.DefCfgData }
+  setmetatable(CfgData, CfgMeta)
+
+  -- Загрузка данных
+  if self:LoadData(CfgData.DataPath) then
+    return true
+  end
+
+  -- Данные по символам
+  self.Data = {
+    Names   = {}, --< NamesList
+    Blocks  = {}, --< Blocks
+  } --
+  local FileName
+
+  -- Разбор имён
+  FileName = sformat("%s%s%s", CfgData.PluginPath,
+                     CfgData.FilePath, CfgData.NamesFile)
+  --logShow(unit.DefCfgData, FileName, "d1")
+  self:ParseNames(FileName)
+
+  -- Разбор имён
+  FileName = sformat("%s%s%s", CfgData.PluginPath,
+                     CfgData.FilePath, CfgData.BlocksFile)
+  --logShow(unit.DefCfgData, FileName, "d1")
+  self:ParseBlocks(FileName)
+
+  FileName = sformat("%s%s%s", CfgData.PluginPath,
+                     CfgData.FilePath, CfgData.DataFile)
+
+  return self:SaveData(FileName)
+end -- Execute
+
+end --
+
+---------------------------------------- run
+if not unit.Data then
+  unit:Execute()
+end
+
+--logShow(unit.Data.Names, "Char Names", "d1")
+--logShow(unit.Data.Blocks, "Char Blocks", "d1")
+
+--------------------------------------------------------------------------------
+return unit.Data
+--------------------------------------------------------------------------------
--- Utils/CharsList.lua
+++ Utils/CharsList.lua
@@ -12,145 +12,32 @@
 --]]
 --------------------------------------------------------------------------------
 
-local tonumber = tonumber
-local setmetatable = setmetatable
-
-local io_open = io.open
-
 ----------------------------------------
 --local context = context
 local logShow = context.ShowInfo
 
-local utils = require 'context.utils.useUtils'
+--local utils = require 'context.utils.useUtils'
 local strings = require 'context.utils.useStrings'
 
 --------------------------------------------------------------------------------
 local unit = {}
 
+unit.Data = require "Rh_Scripts.Utils.CharsData"
+
 ---------------------------------------- Main data
 unit.ScriptName = "CharsList"
 unit.ScriptPath = "scripts\\Rh_Scripts\\Utils\\"
-local PluginPath = utils.PluginPath
-local DataPath = "scripts\\Rh_Scripts\\data\\"
-local DataName = "NamesList.txt"
-
----------------------------------------- ---- Custom
---[==[
-unit.DefCustom = {
-  name = unit.ScriptName,
-  path = unit.ScriptPath,
-
-  label = "CsL",
-
-  help   = { topic = unit.ScriptName, },
-  locale = { kind = 'load', },
-} -- DefCustom
-
-----------------------------------------
---unit.DefOptions = {
---} -- DefOptions
-
----------------------------------------- ---- Config
-
-unit.DefCfgData = { -- Конфигурация по умолчанию:
-  FileName = "NamesList.txt",
-  FilePath = "scripts\\Rh_Scripts\\data\\",
-} -- DefCfgData
-
----------------------------------------- ---- Types
---unit.DlgTypes = {
---} -- DlgTypes
-
----------------------------------------- Main class
-local TMain = {
-  Guid       = win.Uuid(""),
-  ConfigGuid = win.Uuid(""),
-}
-local MMain = { __index = TMain }
-
--- Создание объекта основного класса.
-local function CreateMain (ArgData)
-
-  local self = {
-    ArgData   = addNewData(ArgData, unit.DefCfgData),
-
-    Custom    = false,
-    Options   = false,
-    History   = false,
-    CfgData   = false,
-    --DlgTypes  = unit.DlgTypes,
-    LocData   = false,
-
-    -- Текущее состояние:
-    Current   = false,    -- Текущие данные
-    Names     = {},       -- Список названий
-    --Groups    = false,    --
-  } --- self
-
-  self.ArgData.Custom = self.ArgData.Custom or {} -- MAYBE: addNewData with deep?!
-  --logShow(self.ArgData, "ArgData")
-  self.Custom = datas.customize(self.ArgData.Custom, unit.DefCustom)
-  self.Options = addNewData(self.ArgData.Options, unit.DefOptions)
-
-  self.History = datas.newHistory(self.Custom.history.full)
-  self.CfgData = self.History:field(self.Custom.history.field)
-
-  setmetatable(self.CfgData, { __index = self.ArgData })
-  --logShow(self.CfgData, "CfgData")
-
-  locale.customize(self.Custom)
-  --logShow(self.Custom, "Custom")
-
-  do
-    -- Make and return subtable for t[k].
-    local function subTable (t, k) --> (table)
-      local u = {
-        code = "0000",
-        name = "",
-      } ---
-      t[k] = u
-      return u
-    end -- subTable
-
-    local Names = self.Names
-    Names.__index = subTable; setmetatable(Names, Names)
-  end
-
-  return setmetatable(self, MMain)
-end -- CreateMain
---]==]
-----------------------------------------
-local Names = {} -- Таблица-список названий
-unit.Names = Names
-
--- Make and return subtable for t[k].
-local function subTable (t, k) --> (table)
-  local u = {
-    code = "0000",
-    name = "",
-  } ---
-  t[k] = u
-  return u
-end
-Names.__index = subTable; setmetatable(Names, Names)
+--local PluginPath = utils.PluginPath
 
 ---------------------------------------- Naming
 do --- Преобразование в читаемый формат
 
-  local lower = unicode.utf8.lower
-  local function _EasyName (head, tail) --> (string)
-    return head..lower(tail)
-  end --
+  local CharsData = unit.Data
+  local CharsNames = CharsData and CharsData.Names
 
-function unit.EasyName (s) --> (string)
-  return s:gsub("(%w)(%w+)", _EasyName)
-end -- EasyName
-
-  local CharNames = unit.Names
-  
 -- Получение имени символа по её кодовой точке.
 local function uCPname (c) --> (string)
-  local c = CharNames[c]
+  local c = CharsNames and CharsNames[c]
   return c and c.name or ""
 end -- uCPname
 unit.uCPname = uCPname
@@ -176,79 +63,7 @@ end ---- uCharName
 end --
 ---------------------------------------- main
 
-function unit.Execute (Data) --> (bool | nil)
-
-  local FileName = string.format("%s%s%s", PluginPath, DataPath, DataName)
-
-  local f = io_open(FileName, 'r')
-  if f == nil then return end
-  --[[
-  local f, SError = io_open(FileName, 'r')
-  if SError then logShow(SError, "NamesList.txt") end
-  --]]
-
-  -- Цикл по строкам файла:
-
-  local last, data = 0, 0
-
-  local Names = unit.Names
-  local EasyName = unit.EasyName
-
-  local s = "@"
-  --local sn = 0
-  repeat
-    --logShow(s, "File line")
-    -- Пропуск комментариев, доп. информации и старых названий:
-    while s and (s:find("^%@") or
-          s:find("^\t[^%=]") or
-          s:find("^\t%= .+%(1%.0%)$")) do
-      --logShow(s, "File unused line")
-      s = f:read('*l')
-      --sn = sn + 1
-    end
-    if s == nil then break end -- Конец файла
-    --if sn > 40 then break end -- DEBUG only
-    --logShow(s, "File used line")
-
-    -- TODO: Check codepoint ranges!!
-    -- Основное название:
-    local code, name = s:match("^(%x%x%x%x)\t(.+)$")
-    if code then
-      --logShow(code, name)
-      local cp = tonumber(code, 16)
-      --if cp > 0x300 then break end -- DEBUG only
-      --if cp > 0xFFFF then break end -- DEBUG only
-      last, data = cp, Names[cp]
-      data.code = code
-      data.name = name:sub(1, 1) == '<' and name or EasyName(name)
-    end
-
-    --[[
-    name = s:match("^\t%= (.+)")
-    -- Альтернативное название:
-    if name then
-      data = Names[last]
-      if not data.alias then data.alias = {} end
-      data.alias[#data.alias+1] = name
-    end --
-    --]]
-
-    s = f:read('*l')
-    --sn = sn + 1
-  until s == nil
-
-  f:close()
-
-  return unit
-end -- Execute
-
-if not rawget(Names, 0) then
-  unit.Execute()
-  Names.__index = nil
-  setmetatable(Names, nil)
-end
-
---logShow(Names, "Char Names", "d1")
+--logShow(unit.Data.Names, "Char Names", "d1")
 
 --------------------------------------------------------------------------------
 return unit
