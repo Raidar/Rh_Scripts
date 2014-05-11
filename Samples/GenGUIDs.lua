49c07c751e13b8794c209cc0ab508ba4627ab882
Aidar Rakhmatullin <raidar@yandex.ru>
2014-05-11 13:21:21 +0400

    Sort guids before print

--- Samples/GenGUIDs.lua
+++ Samples/GenGUIDs.lua
@@ -20,7 +20,14 @@ end
 local Count = far.InputBox("Generate GUIDs", "Enter GUID count", nil, "10", 5, nil, 0)
 Count = tonumber(Count) or 10
 
+local t = {}
 for _ = 1, Count do
-  InsText(GenUUID(GenUUID()))
+  t[#t + 1] = GenUUID(GenUUID())
+end
+table.sort(t)
+
+for k = 1, Count do
+  InsText(t[k])
 end
 --------------------------------------------------------------------------------
+
